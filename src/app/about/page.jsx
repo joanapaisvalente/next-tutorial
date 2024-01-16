27f798919c95c508c2e1581ae390d13ba22bd836
Joana Valente <Joana.Valente@ctw.bmwgroup.com>
2024-01-16 15:21:54 +0000

    public images and image config

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className={styles.imageContainer}>
+        <Image
+          src="https://images.pexels.com/photos/19605765/pexels-photo-19605765/free-photo-of-horizon-over-the-ocean-at-dawn.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="about"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
